c1c5ad51f2caa1b8c55b57c07d811cb7e40a68a6
LeTruong2701 <72636049+LeTruong2701@users.noreply.github.com>
2020-10-10 10:11:02 +0700

    dayi=cod

---
+++ TH1.bai1/bai2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TH1.bai1
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private int yearOfBirth;
+        private double salaryLevel;
+        private double basicSalary;
+        public void getId()
+        {
+            Console.WriteLine("Dinh danh nguoi lao dong : ", id);
+        }
+        public void getName()
+        {
+            Console.WriteLine("Ten cua nguoi lao dong : ", name);
+        }
+        public void getYearOfBirth()
+        {
+            Console.WriteLine("Nam sinh nguoi lao dong :", yearOfBirth);
+        }
+        public double getIncome()
+        {
+            double thunhap = salaryLevel * basicSalary;
+            return thunhap;
+        }
+        public void Hienthi()
+        {
+            Console.WriteLine("Dinh danh :"+id+"\t"+"Ten :"+name+"\t"+"Nam sinh:"+yearOfBirth+"\t"+"Luong co ban :"+basicSalary+"\t"+"Thu nhap:"+getIncome());
+        }
+    }
+}
